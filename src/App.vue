9f4f31358dbbe9b9733c6a6d905a429833828547
linuxfight <kiko13024@gmail.com>
2024-05-15 14:34:21 +0300

    add Projects.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
 import Socials from "./components/Socials.vue";
 import Bio from "./components/Bio.vue";
 import Hello from "./components/Hello.vue";
+import Projects from "./components/Projects.vue";
 </script>
 
 <template>
   <div id="app">
-      <Hello/>
-      <Bio/>
-      <Socials/>
+    <Hello/>
+    <Bio/>
+    <Suspense>
+      <Projects/>
+    </Suspense>
+    <Socials/>
   </div>
 </template>
 
---
+++ src/components/Projects.vue
@@ -0,0 +1,16 @@
+<script async setup lang="ts">
+const response = await fetch("https://api.github.com/users/linuxfight/repos");
+const repos = await response.json();
+</script>
+
+<template>
+  <h1> My Projects </h1>
+
+  <li v-for="repo in repos">
+    <a :href="repo.html_url"> {{ repo.name }} </a> - {{ repo.description }}
+  </li>
+</template>
+
+<style scoped>
+
+</style>
